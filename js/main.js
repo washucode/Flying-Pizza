13c125b62dc444a535dddd09cf6d88a81e604d3c
washucode <estherw8525@gmail.com>
2019-07-29 03:34:42 +0300

    add a function to take inputs from form#order

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// business logic
+
 function pizzaOrder (name,size,crust,topping)
 {
   this.name =name;
@@ -10,3 +12,28 @@ pizzaOrder.prototype.total= function(){
   return this.size+this.crust+this.topping;
 
 }
+
+
+// user logic
+
+$(document).ready(function(){
+
+
+
+  $("form#neworder").submit(function(event) {
+    event.preventDefault();
+
+
+    var pizzaType;
+    var sizePrice = 0;
+    var crustPrice = 0;
+    var toppingPrice = 0;
+    var sizeName;
+    var crustName;
+    var toppingsName = ' ';
+    
+  })
+
+
+
+})
